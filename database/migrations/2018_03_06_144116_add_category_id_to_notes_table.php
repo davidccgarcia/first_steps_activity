cb8e02a23d7689799a18b8361d63fc0888035b71
David García <ccristhiangarcia@gmail.com>
2018-03-06 09:50:22 -0500

    Creation of tables with the migration system

---
+++ database/migrations/2018_03_06_144116_add_category_id_to_notes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCategoryIdToNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->unsignedInteger('category_id')->nullable()->index()->after('note');
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->dropColumn('category_id');
+        });
+    }
+}
